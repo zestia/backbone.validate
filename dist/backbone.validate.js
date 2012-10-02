ce969c57a67fc156c0223b800cbc4e3a5a3e9a70
Ross Grayton <rossgrayton@gmail.com>
2012-10-02 09:46:00 +0100

    Refactored code to make more testable

--- dist/backbone.validate.js
+++ dist/backbone.validate.js
@@ -1,159 +1,177 @@
-(function() {
-  var getDisplayMessage, getMessage, hasValue, messages, patterns, validators,
-    _this = this;
+/*! Backbone Validate - v0.1.0 - 2012-10-02
+* https://github.com/zestia/backbone.validate
+* Copyright (c) 2012 Ross Grayton; Licensed MIT */
 
-  Backbone.Model.prototype.validate = function(attrs, options) {
-    var applyValidations, errors, fieldErrors, fieldName, fieldValidations, model, process, processArray, processObject, _ref;
-    errors = {};
-    model = this;
-    process = function(fieldName, fieldValidations, value) {
-      var method;
-      method = _.isArray(value) ? processArray : _.isObject(value) ? processObject : applyValidations;
-      return method(fieldName, fieldValidations, value);
+(function() {
+  var BackboneValidate;
+
+  BackboneValidate = (function() {
+    var _this = this;
+
+    function BackboneValidate(attrs, validations, model) {
+      this.attrs = attrs;
+      this.validations = validations;
+      this.model = model;
+      this.errors = {};
+    }
+
+    BackboneValidate.prototype.validate = function() {
+      var fieldErrors, fieldName, fieldValidations, _ref;
+      if (this.validations != null) {
+        _ref = this.validations;
+        for (fieldName in _ref) {
+          fieldValidations = _ref[fieldName];
+          fieldErrors = this.process(fieldName, fieldValidations, this.attrs[fieldName]);
+          if (!_.isEmpty(fieldErrors)) {
+            this.errors[fieldName] = fieldErrors;
+          }
+        }
+        if (!_.isEmpty(this.errors)) {
+          return this.errors;
+        }
+      }
     };
-    processArray = function(fieldName, fieldValidations, value) {
+
+    BackboneValidate.prototype.process = function(fieldName, fieldValidations, value) {
+      var method;
+      method = _.isArray(value) ? this.processArray : _.isObject(value) ? this.processObject : this.applyValidations;
+      return method.call(this, fieldName, fieldValidations, value);
+    };
+
+    BackboneValidate.prototype.processArray = function(fieldName, fieldValidations, value) {
       var arrayErrors, currentValue, error, index, _i, _len;
       arrayErrors = {};
       for (index = _i = 0, _len = value.length; _i < _len; index = ++_i) {
         currentValue = value[index];
-        error = process(fieldName, fieldValidations, currentValue);
+        error = this.process(fieldName, fieldValidations, currentValue);
         if (!_.isEmpty(error)) {
           arrayErrors[index] = error;
         }
       }
       return arrayErrors;
     };
-    processObject = function(fieldName, fieldValidations, value) {
+
+    BackboneValidate.prototype.processObject = function(fieldName, fieldValidations, value) {
       var error, innerFieldName, innerFieldValidations, objectErrors;
       objectErrors = {};
       for (innerFieldName in fieldValidations) {
         innerFieldValidations = fieldValidations[innerFieldName];
-        error = process(innerFieldName, innerFieldValidations, value[innerFieldName]);
+        error = this.process(innerFieldName, innerFieldValidations, value[innerFieldName]);
         if (!_.isEmpty(error)) {
           objectErrors[innerFieldName] = error;
         }
       }
       return objectErrors;
     };
-    applyValidations = function(fieldName, fieldValidations, value) {
+
+    BackboneValidate.prototype.applyValidations = function(fieldName, fieldValidations, value) {
       var args, error, fieldErrors, option, validationName;
       fieldErrors = {};
       for (validationName in fieldValidations) {
         option = fieldValidations[validationName];
-        args = (option === true ? [] : [option]).concat([value, attrs, model]);
-        error = validators[validationName].apply(null, args);
+        args = (option === true ? [] : [option]).concat([value, this.attrs, this.model]);
+        error = BackboneValidate.validators[validationName].apply(BackboneValidate.validators, args);
         if (error) {
-          fieldErrors[validationName] = getDisplayMessage(error, validationName, option);
+          fieldErrors[validationName] = this.getDisplayMessage(error, validationName, option);
         }
       }
       return fieldErrors;
     };
-    if (this.validations != null) {
-      _ref = this.validations;
-      for (fieldName in _ref) {
-        fieldValidations = _ref[fieldName];
-        fieldErrors = process(fieldName, fieldValidations, attrs[fieldName]);
-        if (!_.isEmpty(fieldErrors)) {
-          errors[fieldName] = fieldErrors;
+
+    BackboneValidate.validators = {
+      hasValue: function(value) {
+        return (value != null) && $.trim(value).length !== 0;
+      },
+      required: function(value, attrs) {
+        return !this.hasValue(value);
+      },
+      min: function(minValue, value, attrs) {
+        if (this.hasValue(value) && value < minValue) {
+          return true;
         }
+      },
+      max: function(maxValue, value, attrs) {
+        if (this.hasValue(value) && value > maxValue) {
+          return true;
+        }
+      },
+      range: function(limits, value, attrs) {
+        return this.hasValue(value) && !((limits[0] <= value && value <= limits[1]));
+      },
+      pattern: function(expr, value, attrs) {
+        if (this.hasValue(value) && !expr.test(value)) {
+          return true;
+        }
+      },
+      email: function(value, attrs) {
+        return this.pattern(BackboneValidate.patterns.email, value, attrs);
+      },
+      url: function(value, attrs) {
+        return this.pattern(BackboneValidate.patterns.url, value, attrs);
+      },
+      custom: function(fn, value, attrs, model) {
+        return fn.call(model, value, attrs);
       }
-      if (!_.isEmpty(errors)) {
-        return errors;
+    };
+
+    BackboneValidate.patterns = {
+      email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
+      url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+    };
+
+    BackboneValidate.messages = {
+      required: 'is required',
+      min: 'must be greater than or equal to {{min}}',
+      max: 'must be less than or equal to {{max}}',
+      range: 'must be between {{0}} and {{1}}',
+      minLength: 'length must be greater than or equal to {{minLength}}',
+      maxLength: 'length must be less than or equal to {{maxLength}}',
+      lengthRange: 'length must be between {{0}} and {{1}}',
+      pattern: 'must match {{pattern}}',
+      email: 'must be a well-formed email address',
+      url: 'must be a well-formed URL',
+      past: 'must be a past date',
+      future: 'must be a future date'
+    };
+
+    BackboneValidate.prototype.getDisplayMessage = function(error, validationName, option) {
+      if (_.isString(error)) {
+        return this.getMessage(error) || error;
+      } else {
+        return this.getMessage(validationName, option);
       }
-    }
-  };
+    };
 
-  hasValue = function(value) {
-    return (value != null) && $.trim(value).length;
-  };
-
-  validators = {
-    required: function(value, attrs) {
-      if (!hasValue(value)) {
-        return true;
+    BackboneValidate.prototype.getMessage = function(validationName, option) {
+      var key, msg, value;
+      msg = BackboneValidate.messages[validationName];
+      if (!msg) {
+        return false;
       }
-    },
-    min: function(minValue, value, attrs) {
-      if (hasValue(value) && value < minValue) {
-        return true;
+      if (!option) {
+        return msg;
       }
-    },
-    max: function(maxValue, value, attrs) {
-      if (hasValue(value) && value > maxValue) {
-        return true;
+      if (_.isObject(option) && !_.isRegExp(option)) {
+        for (key in option) {
+          value = option[key];
+          msg = msg.replace("{{" + key + "}}", value);
+        }
+        return msg;
+      } else {
+        return msg.replace("{{" + validationName + "}}", option);
       }
-    },
-    range: function(limits, value, attrs) {
-      if (hasValue(value) && !((limits[0] <= value && value <= limits[1]))) {
-        return true;
-      }
-    },
-    pattern: function(expr, value, attrs) {
-      if (hasValue(value) && !expr.test(value)) {
-        return true;
-      }
-    },
-    email: function(value, attrs) {
-      return validators.pattern(patterns.email, value, attrs);
-    },
-    url: function(value, attrs) {
-      return validators.pattern(patterns.url, value, attrs);
-    },
-    custom: function(fn, value, attrs, model) {
-      return fn.call(model, value, attrs);
-    }
-  };
+    };
 
-  patterns = {
-    email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
-    url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
-  };
+    return BackboneValidate;
 
-  messages = {
-    required: 'is required',
-    min: 'must be greater than or equal to {{min}}',
-    max: 'must be less than or equal to {{max}}',
-    range: 'must be between {{0}} and {{1}}',
-    minLength: 'length must be greater than or equal to {{minLength}}',
-    maxLength: 'length must be less than or equal to {{maxLength}}',
-    lengthRange: 'length must be between {{0}} and {{1}}',
-    pattern: 'must match {{pattern}}',
-    email: 'must be a well-formed email address',
-    url: 'must be a well-formed URL',
-    past: 'must be a past date',
-    future: 'must be a future date'
-  };
+  }).call(this);
 
-  getDisplayMessage = function(error, validationName, option) {
-    if (_.isString(error)) {
-      return getMessage(error) || error;
-    } else {
-      return getMessage(validationName, option);
-    }
-  };
-
-  getMessage = function(validationName, option) {
-    var key, msg, value;
-    msg = messages[validationName];
-    if (!msg) {
-      return false;
-    }
-    if (!option) {
-      return msg;
-    }
-    if (_.isObject(option) && !_.isRegExp(option)) {
-      for (key in option) {
-        value = option[key];
-        msg = msg.replace("{{" + key + "}}", value);
-      }
-      return msg;
-    } else {
-      return msg.replace("{{" + validationName + "}}", option);
-    }
-  };
-
-  Backbone.Validations = {};
-
-  Backbone.Validations.messages = messages;
+  if (typeof Backbone !== "undefined" && Backbone !== null) {
+    Backbone.Model.prototype.validate = function(attrs, options) {
+      return new BackboneValidate(attrs, this.validations, this).validate();
+    };
+  } else {
+    window.BackboneValidate = BackboneValidate;
+  }
 
 }).call(this);
--- src/backbone.validate.coffee
+++ src/backbone.validate.coffee
@@ -1,113 +1,117 @@
-Backbone.Model::validate = (attrs, options) ->
-  errors = {}
-  model = @
+class BackboneValidate
+  constructor: (@attrs, @validations, @model) ->
+    @errors = {}
 
-  process = (fieldName, fieldValidations, value) ->
+  validate: ->
+    if @validations?
+      for fieldName, fieldValidations of @validations
+        fieldErrors = @process fieldName, fieldValidations, @attrs[fieldName]
+        if not _.isEmpty fieldErrors then @errors[fieldName] = fieldErrors
+      if not _.isEmpty @errors then return @errors
+
+  process: (fieldName, fieldValidations, value) ->
     method = if _.isArray value
-      processArray
+      @processArray
     else if _.isObject value
-      processObject
+      @processObject
     else
-      applyValidations
-    method fieldName, fieldValidations, value
+      @applyValidations
+    method.call @, fieldName, fieldValidations, value
 
-  processArray = (fieldName, fieldValidations, value) ->
+  processArray: (fieldName, fieldValidations, value) ->
     arrayErrors = {}
     for currentValue, index in value
-      error = process fieldName, fieldValidations, currentValue
+      error = @process fieldName, fieldValidations, currentValue
       if not _.isEmpty error then arrayErrors[index] = error
     arrayErrors
 
-  processObject = (fieldName, fieldValidations, value) ->
+  processObject: (fieldName, fieldValidations, value) ->
     objectErrors = {}
     for innerFieldName, innerFieldValidations of fieldValidations
-      error = process innerFieldName, innerFieldValidations, value[innerFieldName]
+      error = @process innerFieldName, innerFieldValidations, value[innerFieldName]
       if not _.isEmpty error then objectErrors[innerFieldName] = error
     objectErrors
 
-  applyValidations = (fieldName, fieldValidations, value) ->
+  applyValidations: (fieldName, fieldValidations, value) ->
     fieldErrors = {}
     for validationName, option of fieldValidations
-      args = (if option is true then [] else [option]).concat([value, attrs, model])
-      error = validators[validationName].apply null, args
-      if error then fieldErrors[validationName] = getDisplayMessage error, validationName, option
+      args = (if option is true then [] else [option]).concat([value, @attrs, @model])
+      error = BackboneValidate.validators[validationName].apply BackboneValidate.validators, args
+      if error then fieldErrors[validationName] = @getDisplayMessage error, validationName, option
     fieldErrors
 
-  if @validations?
-    for fieldName, fieldValidations of @validations
-      fieldErrors = process fieldName, fieldValidations, attrs[fieldName]
-      if not _.isEmpty fieldErrors then errors[fieldName] = fieldErrors
-    if not _.isEmpty errors then return errors
+  @validators:
+    hasValue: (value) ->
+      value? and $.trim(value).length isnt 0
 
-hasValue = (value) ->
-  value? and $.trim(value).length
+    required: (value, attrs) ->
+      not @hasValue(value)
 
-validators = 
-  required: (value, attrs) ->
-    if not hasValue(value) then true
+    min: (minValue, value, attrs) ->
+      if @hasValue(value) and value < minValue then true
 
-  min: (minValue, value, attrs) ->
-    if hasValue(value) and value < minValue then true
+    max: (maxValue, value, attrs) ->
+      if @hasValue(value) and value > maxValue then true
 
-  max: (maxValue, value, attrs) ->
-    if hasValue(value) and value > maxValue then true
+    range: (limits, value, attrs) ->
+      @hasValue(value) and not (limits[0] <= value <= limits[1])
 
-  range: (limits, value, attrs) ->
-    if hasValue(value) and not (limits[0] <= value <= limits[1]) then true
+    pattern: (expr, value, attrs) ->
+      if @hasValue(value) and not expr.test(value) then true
 
-  pattern: (expr, value, attrs) ->
-    if hasValue(value) and not expr.test(value) then true
+    email: (value, attrs) ->
+      @pattern BackboneValidate.patterns.email, value, attrs
 
-  email: (value, attrs) ->
-    validators.pattern patterns.email, value, attrs
+    url: (value, attrs) ->
+      @pattern BackboneValidate.patterns.url, value, attrs
 
-  url: (value, attrs) ->
-    validators.pattern patterns.url, value, attrs
+    custom: (fn, value, attrs, model) =>
+      fn.call model, value, attrs
 
-  custom: (fn, value, attrs, model) =>
-    fn.call model, value, attrs
+    # TODO:
+    #   minlength
+    #   maxlength
+    #   lengthRange
+    #   number
+    #   in
 
-  # TODO:
-  #   minlength
-  #   maxlength
-  #   lengthRange
-  #   number
-  #   in
+  @patterns:
+    email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
+    url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
 
-patterns =
-  email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
-  url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+  @messages:
+    required: 'is required'
+    min: 'must be greater than or equal to {{min}}'
+    max: 'must be less than or equal to {{max}}'
+    range: 'must be between {{0}} and {{1}}'
 
-messages =
-  required: 'is required'
-  min: 'must be greater than or equal to {{min}}'
-  max: 'must be less than or equal to {{max}}'
-  range: 'must be between {{0}} and {{1}}'
+    minLength: 'length must be greater than or equal to {{minLength}}'
+    maxLength: 'length must be less than or equal to {{maxLength}}'
+    lengthRange: 'length must be between {{0}} and {{1}}'
 
-  minLength: 'length must be greater than or equal to {{minLength}}'
-  maxLength: 'length must be less than or equal to {{maxLength}}'
-  lengthRange: 'length must be between {{0}} and {{1}}'
+    pattern: 'must match {{pattern}}'
+    email: 'must be a well-formed email address'
+    url: 'must be a well-formed URL'
 
-  pattern: 'must match {{pattern}}'
-  email: 'must be a well-formed email address'
-  url: 'must be a well-formed URL'
+    past: 'must be a past date'
+    future: 'must be a future date'
 
-  past: 'must be a past date'
-  future: 'must be a future date'
+  getDisplayMessage: (error, validationName, option) ->
+    if _.isString error then @getMessage(error) or error else @getMessage validationName, option
 
-getDisplayMessage = (error, validationName, option) ->
-  if _.isString error then getMessage(error) or error else getMessage validationName, option
+  getMessage: (validationName, option) ->
+    msg = BackboneValidate.messages[validationName]
+    if not msg then return false
+    if not option then return msg
 
-getMessage = (validationName, option) ->
-  msg = messages[validationName]
-  if not msg then return false
-  if not option then return msg
+    if _.isObject(option) and not _.isRegExp(option)
+      msg = msg.replace "{{#{key}}}", value for key, value of option
+      msg
+    else
+      msg.replace "{{#{validationName}}}", option
 
-  if _.isObject(option) and not _.isRegExp(option)
-    msg = msg.replace "{{#{key}}}", value for key, value of option
-    msg
-  else
-    msg.replace "{{#{validationName}}}", option
-
-Backbone.Validations = {}
-Backbone.Validations.messages = messages
+if Backbone?
+  Backbone.Model::validate = (attrs, options) ->
+    new BackboneValidate(attrs, @validations, @).validate()
+else
+  window.BackboneValidate = BackboneValidate
